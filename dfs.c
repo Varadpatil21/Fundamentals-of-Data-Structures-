446ed31cb36fbd2693730ce865b7fcea2a8383da
Varadpatil21 <96022723+Varadpatil21@users.noreply.github.com>
2023-03-23 17:05:18 +0530

    Add files via upload

--- dfs.c
+++ dfs.c
@@ -2,10 +2,11 @@
 #define n 5
 int A[n][n];
 int visited[n];
-
+int stack[20];
+int top=-1;
 void DFS(int v)
 {
-	visited[v]=1;
+	visited[v]=1;5
 	printf("%d ",v);
 	for(int i=0;i<n;i++)
 	{
@@ -16,6 +17,32 @@ void DFS(int v)
 		}
 	}
 		
+}
+
+void DFS1(int v)
+{
+	visited[v]=1;
+	printf("%d ",v);
+	for(int i=0;i<n;i++)
+	{
+		if(A[v][i]==1 && !visited[i])
+		{
+			top++;
+			stack[top]=i;
+			DFS1(i);
+			break;
+		}
+	}	
+	while(top!=-1)
+	{
+		for(int i=0;i<n;i++)
+		{
+			if(A[stack[top]][i]==1 && !visited[i])
+				DFS1(i);
+			else
+				top--;
+		}
+	}
 }
 
 int main()
@@ -35,5 +62,7 @@ int main()
 	}
 	printf("Enter the source point:");
 	scanf("%d",&s);
-	DFS(s);
+	top++;
+	stack[top]=s;
+	DFS1(s);
 }
